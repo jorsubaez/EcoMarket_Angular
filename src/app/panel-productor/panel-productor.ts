42a95ad7becbc7ad4f6529e3d480f0ba37e82517
jorge <jorgesuarezbaez@gmail.com>
2026-04-20 17:54:55 +0100

    First commit

---
+++ src/app/panel-productor/panel-productor.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-productor',
+  imports: [],
+  templateUrl: './panel-productor.html',
+  styleUrl: './panel-productor.css',
+})
+export class PanelProductor {}
---
+++ src/app/productores/productores.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productores',
+  imports: [],
+  templateUrl: './productores.html',
+  styleUrl: './productores.css',
+})
+export class Productores {}
